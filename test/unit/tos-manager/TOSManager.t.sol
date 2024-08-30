66fa909f31d96f138e8713f7c0c05da61899067e
Sam Bugs <101145325+0xsambugs@users.noreply.github.com>
2024-08-30 11:51:13 -0300

    feat: add tos manager (#26)

---
+++ test/unit/tos-manager/TOSManager.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.22;
+
+// solhint-disable no-unused-import
+import { PRBTest } from "@prb/test/PRBTest.sol";
+import { TOSManager, StrategyId } from "../../../src/tos-manager/TOSManager.sol";
+import { CommonUtils } from "../../utils/CommonUtils.sol";
+import { IAccessControl } from "@openzeppelin/contracts/access/extensions/IAccessControlDefaultAdminRules.sol";
+
+contract TosManagerTest is PRBTest {
+  address private superAdmin = address(1);
+  address private manageTosAdmin = address(2);
+  TOSManager private tosManager;
+
+  function setUp() public virtual {
+    tosManager = new TOSManager(superAdmin, CommonUtils.arrayOf(manageTosAdmin));
+  }
+
+  function test_constants() public {
+    assertEq(tosManager.MANAGE_TOS_ROLE(), keccak256("MANAGE_TOS_ROLE"));
+  }
+
+  function test_constructor() public {
+    assertTrue(tosManager.hasRole(tosManager.MANAGE_TOS_ROLE(), manageTosAdmin));
+
+    // Access control
+    assertEq(tosManager.defaultAdminDelay(), 3 days);
+    assertEq(tosManager.owner(), superAdmin);
+    assertEq(tosManager.defaultAdmin(), superAdmin);
+  }
+}
