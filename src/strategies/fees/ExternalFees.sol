2fad318f13e563b07d239fd85e0da07ee9bc36ec
Sam Bugs <sambugs@protonmail.com>
2024-09-13 16:25:49 -0300

    feat: implement fees deposited

--- src/strategies/fees/ExternalFees.sol
+++ src/strategies/fees/ExternalFees.sol
@@ -20,11 +20,27 @@ abstract contract ExternalFees is BaseFees, Initializable {
   /// @notice The id for the Fee Manager
   bytes32 public constant FEE_MANAGER = keccak256("FEE_MANAGER");
 
+  mapping(address token => PerformanceData data) private _performanceData;
+
   /// @notice The address of the global registry
   function globalRegistry() public view virtual returns (IGlobalEarnRegistry);
 
   /// @notice The id assigned to this strategy
   function strategyId() public view virtual returns (StrategyId);
+
+  /// @notice Returns the amount of collected fees
+  function collectedFees() public view returns (address[] memory tokens, uint256[] memory collected) {
+    Fees memory fees = _getFees();
+    uint256[] memory balances;
+    (tokens, balances) = _fees_underlying_totalBalances();
+    collected = new uint256[](tokens.length);
+    for (uint256 i = 0; i < tokens.length; i++) {
+      collected[i] = _calculateFees(tokens[i], balances[i], fees.performanceFee);
+    }
+  }
+
+  // slither-disable-next-line naming-convention
+  function _fees_underlying_asset() internal view virtual returns (address asset);
 
   // slither-disable-next-line naming-convention,dead-code,assembly
   function _fees_fees() internal view override returns (IEarnStrategy.FeeType[] memory types, uint16[] memory bps) {
@@ -67,7 +83,29 @@ abstract contract ExternalFees is BaseFees, Initializable {
     internal
     override
     returns (uint256 assetsDeposited)
-  { }
+  {
+    Fees memory fees = _getFees();
+    if (fees.performanceFee == 0) {
+      // If performance fee is 0, we will need to clear the last balance. Otherwise, once it's turned on again,
+      // we won't be able to understand different between balance changes and yield
+      address asset = _fees_underlying_asset();
+      _clearBalanceIfSet(asset);
+      return _fees_underlying_deposited(depositToken, depositAmount);
+    }
+
+    // Note: we are only updating fees for the asset, since it's the only token whose balance will change
+    (address[] memory tokens, uint256[] memory currentBalances) = _fees_underlying_totalBalances();
+    uint256 performanceFees = _calculateFees(tokens[0], currentBalances[0], fees.performanceFee);
+
+    assetsDeposited = _fees_underlying_deposited(depositToken, depositAmount);
+
+    _performanceData[tokens[0]] = PerformanceData({
+      // Note: there might be a small wei difference here, but we can ignore it since it should be negligible
+      lastBalance: (currentBalances[0] + assetsDeposited).toUint128(),
+      performanceFees: performanceFees.toUint120(),
+      isSet: true
+    });
+  }
 
   // slither-disable-next-line naming-convention,dead-code
   function _fees_withdraw(
@@ -98,6 +136,26 @@ abstract contract ExternalFees is BaseFees, Initializable {
       bytes memory result
     )
   { }
+
+  // slither-disable-next-line dead-code
+  function _clearBalanceIfSet(address token) private {
+    PerformanceData memory tokenPerfData = _performanceData[token];
+    if (tokenPerfData.isSet) {
+      _performanceData[token] =
+        PerformanceData({ lastBalance: 0, isSet: false, performanceFees: tokenPerfData.performanceFees });
+    }
+  }
+
+  // slither-disable-next-line dead-code
+  function _calculateFees(address token, uint256 currentBalance, uint256 performanceFee) private view returns (uint256) {
+    PerformanceData memory perfData = _performanceData[token];
+    if (perfData.isSet && currentBalance > perfData.lastBalance) {
+      uint256 yield = currentBalance - perfData.lastBalance;
+      uint256 fee = (yield * performanceFee) / 10_000;
+      return fee + perfData.performanceFees;
+    }
+    return 0;
+  }
 
   // slither-disable-next-line dead-code
   function _getFees() private view returns (Fees memory fees) {
--- test/unit/strategies/fees/ExternalFees.t.sol
+++ test/unit/strategies/fees/ExternalFees.t.sol
@@ -17,11 +17,16 @@ contract ExternalFeesTest is Test {
   ExternalFeesInstance private fees;
   IGlobalEarnRegistry private registry = IGlobalEarnRegistry(address(1));
   IFeeManager private manager = IFeeManager(address(2));
+  address private asset = address(3);
+  address private token = address(4);
   StrategyId private strategyId = StrategyId.wrap(1);
   VmSafe.Wallet private alice = vm.createWallet("alice");
 
   function setUp() public virtual {
-    fees = new ExternalFeesInstance(registry, strategyId);
+    address[] memory tokens = new address[](2);
+    tokens[0] = asset;
+    tokens[1] = token;
+    fees = new ExternalFeesInstance(registry, strategyId, tokens);
     vm.mockCall(
       address(registry),
       abi.encodeWithSelector(IGlobalEarnRegistry.getAddressOrFail.selector, keccak256("FEE_MANAGER")),
@@ -43,19 +48,66 @@ contract ExternalFeesTest is Test {
     assertEq(bps[0], 100);
     assertEq(bps[1], 300);
   }
+
+  function test_deposited() public {
+    _setFee(500); // 5%
+
+    // Deposit 100k
+    fees.deposited(asset, 100_000);
+
+    // Set balance to 150k (so yield was 50k)
+    fees.setBalance(asset, 150_000);
+
+    (address[] memory tokens, uint256[] memory collected) = fees.collectedFees();
+    assertEq(tokens.length, 2);
+    assertEq(collected.length, 2);
+    assertEq(tokens[0], asset);
+    assertEq(tokens[1], token);
+    assertEq(collected[0], 2500);
+    assertEq(collected[1], 0);
+
+    // Deposit another 100k (balance is now 250k)
+    fees.deposited(asset, 100_000);
+
+    // Set balance to 300k (so yield was 100k)
+    fees.setBalance(asset, 300_000);
+
+    (tokens, collected) = fees.collectedFees();
+    assertEq(tokens.length, 2);
+    assertEq(collected.length, 2);
+    assertEq(tokens[0], asset);
+    assertEq(tokens[1], token);
+    assertEq(collected[0], 5000);
+    assertEq(collected[1], 0);
+  }
+
+  function _setFee(uint16 bps) private {
+    vm.mockCall(
+      address(manager),
+      abi.encodeWithSelector(IFeeManager.getFees.selector, strategyId),
+      abi.encode(Fees({ depositFee: 0, withdrawFee: 0, performanceFee: bps, rescueFee: 0 }))
+    );
+  }
 }
 
 contract ExternalFeesInstance is ExternalFees {
   IGlobalEarnRegistry private _registry;
   StrategyId private _strategyId;
+  address[] private _tokens;
+  mapping(address token => uint256 balance) private _balances;
 
-  constructor(IGlobalEarnRegistry registry, StrategyId strategyId_) {
+  constructor(IGlobalEarnRegistry registry, StrategyId strategyId_, address[] memory tokens) {
     _registry = registry;
     _strategyId = strategyId_;
+    _tokens = tokens;
   }
 
   function fees() external view returns (IEarnStrategy.FeeType[] memory types, uint16[] memory bps) {
     return _fees_fees();
+  }
+
+  function deposited(address token, uint256 amount) external returns (uint256) {
+    return _fees_deposited(token, amount);
   }
 
   function globalRegistry() public view override returns (IGlobalEarnRegistry) {
@@ -66,21 +118,34 @@ contract ExternalFeesInstance is ExternalFees {
     return _strategyId;
   }
 
+  function setBalance(address token, uint256 balance) external {
+    _balances[token] = balance;
+  }
+
   function _fees_underlying_totalBalances()
     internal
     view
     override
     returns (address[] memory tokens, uint256[] memory balances)
-  { }
+  {
+    tokens = _tokens;
+    balances = new uint256[](tokens.length);
+    for (uint256 i = 0; i < tokens.length; i++) {
+      balances[i] = _balances[tokens[i]];
+    }
+  }
 
   function _fees_underlying_deposited(
-    address depositToken,
+    address,
     uint256 depositAmount
   )
     internal
+    pure
     override
     returns (uint256 assetsDeposited)
-  { }
+  {
+    return depositAmount;
+  }
 
   function _fees_underlying_withdraw(
     uint256 positionId,
@@ -109,4 +174,8 @@ contract ExternalFeesInstance is ExternalFees {
       bytes memory result
     )
   { }
+
+  function _fees_underlying_asset() internal view override returns (address asset) {
+    return _tokens[0];
+  }
 }
