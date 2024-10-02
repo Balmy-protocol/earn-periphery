6d966eb0f5e41121c7f7338f204fb1509d534cda
0xKoaj <141654454+0xKoaj@users.noreply.github.com>
2024-10-02 15:28:50 -0300

    feat: implement setCampaign in manager (#75)
    * feat: implement setCampaign in manager
    
    * chore: fix lint
    
    * feat: implement rewards and rewardAmount
    
    * chore: modify visibility
    
    * refactor: remove claimed, fix test and slither
    
    * refactor: some suggestions
    
    * refactor: reading variable from mem
    
    * refactor: some suggestions

---
+++ src/liquidity-mining-manager/LiquidityMiningManager.sol
@@ -0,0 +1,155 @@
+// SPDX-License-Identifier: TBD
+pragma solidity >=0.8.22;
+
+import { AccessControlDefaultAdminRules } from
+  "@openzeppelin/contracts/access/extensions/AccessControlDefaultAdminRules.sol";
+import { IEarnStrategy, StrategyId, IEarnStrategyRegistry } from "@balmy/earn-core/interfaces/IEarnStrategy.sol";
+import { ILiquidityMiningManager, ILiquidityMiningManagerCore } from "../interfaces/ILiquidityMiningManager.sol";
+import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { Token } from "@balmy/earn-core/libraries/Token.sol";
+import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
+import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
+/**
+ * @notice A liquidity mining manager that allows the configuration of liquidity mining campaigns
+ */
+contract LiquidityMiningManager is ILiquidityMiningManager, AccessControlDefaultAdminRules {
+  using SafeERC20 for IERC20;
+  using Math for uint256;
+  using SafeCast for uint256;
+  using Token for address;
+
+  error UnauthorizedCaller();
+  error InvalidReward();
+  error InsufficientBalance();
+
+  struct Campaign {
+    uint88 emissionPerSecond;
+    uint32 deadline;
+    uint104 pendingFromLastUpdate;
+    uint32 lastUpdated;
+  }
+
+  /// @inheritdoc ILiquidityMiningManager
+  bytes32 public constant MANAGE_CAMPAIGNS_ROLE = keccak256("MANAGE_CAMPAIGNS_ROLE");
+
+  /// @inheritdoc ILiquidityMiningManager
+  // slither-disable-next-line naming-convention
+  IEarnStrategyRegistry public immutable STRATEGY_REGISTRY;
+
+  mapping(bytes32 strategyAndReward => Campaign campaign) internal _campaigns;
+
+  mapping(StrategyId strategyId => address[] rewards) internal _rewards;
+
+  constructor(
+    IEarnStrategyRegistry registry,
+    address superAdmin,
+    address[] memory initialAdmins
+  )
+    AccessControlDefaultAdminRules(3 days, superAdmin)
+  {
+    STRATEGY_REGISTRY = registry;
+    _assignRoles(MANAGE_CAMPAIGNS_ROLE, initialAdmins);
+  }
+
+  /// @inheritdoc ILiquidityMiningManagerCore
+  function rewardAmount(StrategyId strategyId, address token) external view override returns (uint256) {
+    Campaign memory campaign = _campaigns[_key(strategyId, token)];
+    return _calculateRewardAmount(campaign);
+  }
+
+  /// @inheritdoc ILiquidityMiningManagerCore
+  function rewards(StrategyId strategyId) external view override returns (address[] memory) {
+    return _rewards[strategyId];
+  }
+
+  /// @inheritdoc ILiquidityMiningManagerCore
+  // solhint-disable-next-line no-empty-blocks
+  function claim(StrategyId strategyId, address token, uint256 amount, address recipient) external override { }
+
+  /// @inheritdoc ILiquidityMiningManagerCore
+  // solhint-disable-next-line no-empty-blocks
+  function deposited(StrategyId strategyId, uint256 assetsDeposited) external override {
+    // Does nothing, but we we want to have this function for future liquidity mining manager implementations
+  }
+
+  /// @inheritdoc ILiquidityMiningManagerCore
+  // solhint-disable-next-line no-empty-blocks
+  function withdrew(StrategyId strategyId, uint256 assetsWithdrawn) external override {
+    // Does nothing, but we we want to have this function for future liquidity mining manager implementations
+  }
+
+  /// @inheritdoc ILiquidityMiningManagerCore
+  // solhint-disable-next-line no-empty-blocks
+  function strategySelfConfigure(bytes calldata data) external override { }
+
+  /// @inheritdoc ILiquidityMiningManager
+  //slither-disable-start timestamp
+  //slither-disable-next-line reentrancy-no-eth
+  function setCampaign(
+    StrategyId strategyId,
+    address reward,
+    uint256 emissionPerSecond,
+    uint256 deadline
+  )
+    external
+    payable
+    override
+    onlyRole(MANAGE_CAMPAIGNS_ROLE)
+  {
+    bytes32 key = _key(strategyId, reward);
+    Campaign storage campaign = _campaigns[key];
+    Campaign memory campaignMem = campaign;
+
+    if (campaignMem.lastUpdated == 0) {
+      IEarnStrategy strategy = STRATEGY_REGISTRY.getStrategy(strategyId);
+      if (strategy.asset() == reward) {
+        revert InvalidReward();
+      }
+      _rewards[strategyId].push(reward);
+    } else {
+      // Update the pending rewards
+      campaign.pendingFromLastUpdate = (_calculateRewardAmount(campaign)).toUint104();
+    }
+
+    uint256 balanceNeeded = emissionPerSecond * (deadline - block.timestamp);
+    uint256 currentBalance = (campaignMem.deadline > block.timestamp)
+      ? campaignMem.emissionPerSecond * (campaignMem.deadline - block.timestamp)
+      : 0;
+    if (currentBalance < balanceNeeded) {
+      // Transfer the missing tokens
+      if (reward == Token.NATIVE_TOKEN) {
+        if (msg.value != balanceNeeded - currentBalance) {
+          revert InsufficientBalance();
+        }
+      } else {
+        IERC20(reward).safeTransferFrom(msg.sender, address(this), balanceNeeded - currentBalance);
+      }
+    } else if (currentBalance > balanceNeeded) {
+      // Return the excess tokens
+      // slither-disable-next-line arbitrary-send-eth,reentrancy-eth,reentrancy-events,reentrancy-unlimited-gas
+      reward.transfer({ recipient: msg.sender, amount: currentBalance - balanceNeeded });
+    }
+
+    campaign.emissionPerSecond = emissionPerSecond.toUint88();
+    campaign.deadline = deadline.toUint32();
+    campaign.lastUpdated = block.timestamp.toUint32();
+    emit CampaignSet(strategyId, reward, emissionPerSecond, deadline);
+  }
+  //slither-disable-end timestamp
+
+  function _assignRoles(bytes32 role, address[] memory accounts) internal {
+    for (uint256 i; i < accounts.length; ++i) {
+      _grantRole(role, accounts[i]);
+    }
+  }
+
+  function _key(StrategyId strategyId, address token) internal pure returns (bytes32) {
+    return keccak256(abi.encodePacked(strategyId, token));
+  }
+
+  function _calculateRewardAmount(Campaign memory campaign) internal view returns (uint256) {
+    return campaign.pendingFromLastUpdate
+      + campaign.emissionPerSecond * (Math.min(block.timestamp, campaign.deadline) - campaign.lastUpdated);
+  }
+}
---
+++ test/unit/liquidity-mining-manager/LiquidityMiningManager.t.sol
@@ -0,0 +1,331 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.22;
+
+// solhint-disable no-unused-import
+import { PRBTest } from "@prb/test/PRBTest.sol";
+import { ERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import {
+  LiquidityMiningManager,
+  ILiquidityMiningManager,
+  StrategyId,
+  IEarnStrategyRegistry,
+  IEarnStrategy
+} from "../../../src/liquidity-mining-manager/LiquidityMiningManager.sol";
+import { CommonUtils } from "../../utils/CommonUtils.sol";
+import { IAccessControl } from "@openzeppelin/contracts/access/extensions/IAccessControlDefaultAdminRules.sol";
+import { ERC20MintableBurnableMock } from "@balmy/earn-core-test/mocks/ERC20/ERC20MintableBurnableMock.sol";
+import { Token } from "@balmy/earn-core/libraries/Token.sol";
+import { StdCheats } from "forge-std/StdCheats.sol";
+
+contract LiquidityMiningManagerTest is PRBTest, StdCheats {
+  event CampaignSet(StrategyId indexed strategyId, address indexed reward, uint256 emissionPerSecond, uint256 deadline);
+
+  address private superAdmin = address(1);
+  address private adminManageCampaigns = address(2);
+  StrategyId private strategyId = StrategyId.wrap(1);
+  IEarnStrategy private strategy = IEarnStrategy(address(3));
+  IEarnStrategyRegistry private registry = IEarnStrategyRegistry(address(6));
+  LiquidityMiningManager private manager;
+  IERC20 private asset = IERC20(address(7));
+  ERC20MintableBurnableMock private reward = new ERC20MintableBurnableMock();
+  ERC20MintableBurnableMock private anotherReward = new ERC20MintableBurnableMock();
+
+  function setUp() public virtual {
+    manager = new LiquidityMiningManager(registry, superAdmin, CommonUtils.arrayOf(adminManageCampaigns));
+    reward.mint(address(adminManageCampaigns), type(uint256).max);
+    anotherReward.mint(address(adminManageCampaigns), type(uint256).max);
+    vm.deal(address(adminManageCampaigns), type(uint256).max);
+
+    vm.mockCall(
+      address(registry),
+      abi.encodeWithSelector(IEarnStrategyRegistry.getStrategy.selector, strategyId),
+      abi.encode(address(strategy))
+    );
+    vm.mockCall(address(strategy), abi.encodeWithSelector(IEarnStrategy.asset.selector), abi.encode(address(asset)));
+  }
+
+  function test_constants() public {
+    assertEq(manager.MANAGE_CAMPAIGNS_ROLE(), keccak256("MANAGE_CAMPAIGNS_ROLE"));
+  }
+
+  function test_constructor() public {
+    assertEq(address(manager.STRATEGY_REGISTRY()), address(registry));
+
+    assertTrue(manager.hasRole(manager.MANAGE_CAMPAIGNS_ROLE(), adminManageCampaigns));
+
+    // Access control
+    assertEq(manager.defaultAdminDelay(), 3 days);
+    assertEq(manager.owner(), superAdmin);
+    assertEq(manager.defaultAdmin(), superAdmin);
+  }
+
+  function test_strategySelfConfigure_emptyBytes() public {
+    // Nothing happens
+    manager.strategySelfConfigure("");
+  }
+
+  function test_setCampaign_firstCampaign() public {
+    vm.startPrank(adminManageCampaigns);
+    uint256 balanceForCampaign = 3 * 10;
+    reward.approve(address(manager), balanceForCampaign);
+    uint256 previousBalance = reward.balanceOf(address(adminManageCampaigns));
+    vm.expectEmit();
+    emit CampaignSet(strategyId, address(reward), 3, block.timestamp + 10);
+    // Make sure it can be called without reverting
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: address(reward),
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 10
+    });
+    vm.stopPrank();
+    assertEq(reward.balanceOf(address(adminManageCampaigns)), previousBalance - balanceForCampaign);
+    assertEq(reward.balanceOf(address(manager)), balanceForCampaign);
+    assertEq(manager.rewards(strategyId)[0], address(reward));
+  }
+
+  function test_setCampaign_twoCampaigns() public {
+    vm.startPrank(adminManageCampaigns);
+    uint256 balanceForCampaign = 3 * 10;
+    reward.approve(address(manager), balanceForCampaign);
+    anotherReward.approve(address(manager), balanceForCampaign);
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: address(reward),
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 10
+    });
+
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: address(anotherReward),
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 10
+    });
+    vm.stopPrank();
+    assertEq(manager.rewards(strategyId)[0], address(reward));
+    assertEq(manager.rewards(strategyId)[1], address(anotherReward));
+  }
+
+  function test_setCampaign_modifyCampaign_addBalance() public {
+    vm.startPrank(adminManageCampaigns);
+    uint256 balanceForCampaign = 3 * 10;
+    reward.approve(address(manager), balanceForCampaign);
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: address(reward),
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 10
+    });
+
+    balanceForCampaign = 3 * 100 - balanceForCampaign;
+    uint256 previousBalance = reward.balanceOf(address(adminManageCampaigns));
+    reward.approve(address(manager), balanceForCampaign);
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: address(reward),
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 100
+    });
+    vm.stopPrank();
+    assertEq(reward.balanceOf(address(adminManageCampaigns)), previousBalance - balanceForCampaign);
+    assertEq(reward.balanceOf(address(manager)), balanceForCampaign + 3 * 10);
+  }
+
+  function test_setCampaign_modifyCampaign_getBalance() public {
+    vm.startPrank(adminManageCampaigns);
+    uint256 balanceForCampaign = 3 * 10;
+    reward.approve(address(manager), balanceForCampaign);
+
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: address(reward),
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 10
+    });
+    uint256 previousBalance = reward.balanceOf(address(adminManageCampaigns));
+
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: address(reward),
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 5
+    });
+    vm.stopPrank();
+    assertEq(reward.balanceOf(address(adminManageCampaigns)), previousBalance + 3 * 5);
+  }
+
+  function test_setCampaign_firstCampaign_Native() public {
+    vm.startPrank(adminManageCampaigns);
+    uint256 balanceForCampaign = 3 * 10;
+    uint256 previousBalance = address(adminManageCampaigns).balance;
+    vm.expectEmit();
+    emit CampaignSet(strategyId, Token.NATIVE_TOKEN, 3, block.timestamp + 10);
+    // Make sure it can be called without reverting
+    manager.setCampaign{ value: balanceForCampaign }({
+      strategyId: strategyId,
+      reward: Token.NATIVE_TOKEN,
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 10
+    });
+    vm.stopPrank();
+    assertEq(address(adminManageCampaigns).balance, previousBalance - balanceForCampaign);
+    assertEq(address(manager).balance, balanceForCampaign);
+  }
+
+  function test_setCampaign_modifyCampaign_addBalance_Native() public {
+    vm.startPrank(adminManageCampaigns);
+    uint256 balanceForCampaign = 3 * 10;
+    manager.setCampaign{ value: balanceForCampaign }({
+      strategyId: strategyId,
+      reward: Token.NATIVE_TOKEN,
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 10
+    });
+
+    balanceForCampaign = 3 * 100 - balanceForCampaign;
+    uint256 previousBalance = address(adminManageCampaigns).balance;
+    manager.setCampaign{ value: balanceForCampaign }({
+      strategyId: strategyId,
+      reward: Token.NATIVE_TOKEN,
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 100
+    });
+    vm.stopPrank();
+    assertEq(address(adminManageCampaigns).balance, previousBalance - balanceForCampaign);
+    assertEq(address(manager).balance, balanceForCampaign + 3 * 10);
+  }
+
+  function test_setCampaign_modifyCampaign_getBalance_Native() public {
+    vm.startPrank(adminManageCampaigns);
+    uint256 balanceForCampaign = 3 * 10;
+
+    manager.setCampaign{ value: balanceForCampaign }({
+      strategyId: strategyId,
+      reward: Token.NATIVE_TOKEN,
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 10
+    });
+    uint256 previousBalance = address(adminManageCampaigns).balance;
+
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: Token.NATIVE_TOKEN,
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 5
+    });
+    vm.stopPrank();
+    assertEq(address(adminManageCampaigns).balance, previousBalance + 3 * 5);
+  }
+
+  function test_setCampaign_RevertWhen_rewardIsStrategyAsset() public {
+    vm.prank(adminManageCampaigns);
+    vm.expectRevert(abi.encodeWithSelector(LiquidityMiningManager.InvalidReward.selector));
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: address(asset),
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 1 days
+    });
+  }
+
+  function test_setCampaign_RevertWhen_Native_InsufficientBalance() public {
+    vm.prank(adminManageCampaigns);
+    vm.expectRevert(abi.encodeWithSelector(LiquidityMiningManager.InsufficientBalance.selector));
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: Token.NATIVE_TOKEN,
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 1 days
+    });
+  }
+
+  function test_rewardAmounts_twoCampaigns() public {
+    uint256 timestamp = 10;
+    vm.warp(timestamp);
+
+    vm.startPrank(adminManageCampaigns);
+    uint256 balanceForCampaign = 3 * 10;
+    reward.approve(address(manager), balanceForCampaign);
+    anotherReward.approve(address(manager), balanceForCampaign);
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: address(reward),
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 10
+    });
+
+    assertEq(manager.rewardAmount(strategyId, address(reward)), 0); // No rewards yet
+
+    timestamp += 5; // 5 seconds passed
+    vm.warp(timestamp);
+
+    assertEq(manager.rewardAmount(strategyId, address(reward)), 3 * 5);
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: address(anotherReward),
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 10
+    });
+
+    assertEq(manager.rewardAmount(strategyId, address(anotherReward)), 0); // No rewards yet
+
+    timestamp += 1000; // 1000 seconds passed, deadline reached for both campaigns
+    vm.warp(timestamp);
+
+    assertEq(manager.rewardAmount(strategyId, address(reward)), 3 * 10);
+    assertEq(manager.rewardAmount(strategyId, address(anotherReward)), 3 * 10);
+    vm.stopPrank();
+  }
+
+  function test_rewardAmount_modifyCampaign_addBalance() public {
+    uint256 timestamp = 10; // Start at 10 seconds
+    vm.warp(timestamp);
+
+    vm.startPrank(adminManageCampaigns);
+    uint256 balanceForCampaign = 3 * 10;
+    reward.approve(address(manager), balanceForCampaign);
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: address(reward),
+      emissionPerSecond: 3,
+      deadline: block.timestamp + 10
+    });
+
+    timestamp += 5; // 5 seconds passed
+    vm.warp(timestamp); // 5 seconds passed
+
+    assertEq(manager.rewardAmount(strategyId, address(reward)), 3 * 5);
+    balanceForCampaign = 10 * 100 - 3 * 5;
+    reward.approve(address(manager), balanceForCampaign);
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: address(reward),
+      emissionPerSecond: 10,
+      deadline: block.timestamp + 100
+    });
+
+    assertEq(manager.rewardAmount(strategyId, address(reward)), 3 * 5);
+
+    timestamp += 10;
+    vm.warp(timestamp); // 10 seconds passed
+
+    assertEq(manager.rewardAmount(strategyId, address(reward)), 3 * 5 + 10 * 10);
+
+    balanceForCampaign = 5 * 100;
+    reward.approve(address(manager), balanceForCampaign);
+    manager.setCampaign({
+      strategyId: strategyId,
+      reward: address(reward),
+      emissionPerSecond: 5,
+      deadline: block.timestamp + 100
+    });
+
+    timestamp += 15;
+    vm.warp(timestamp); // 15 seconds passed
+    assertEq(manager.rewardAmount(strategyId, address(reward)), 3 * 5 + 10 * 10 + 5 * 15);
+
+    vm.stopPrank();
+  }
+}
