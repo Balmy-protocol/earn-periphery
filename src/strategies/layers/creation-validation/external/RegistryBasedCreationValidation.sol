54d5bad08ea2e3b1cb9414afc4612cf8ba13aae3
Sam Bugs <sambugs@protonmail.com>
2024-12-17 16:13:07 -0300

    feat: add registry based validation

---
+++ src/strategies/layers/creation-validation/external/RegistryBasedCreationValidation.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.22;
+
+import { Initializable } from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import { IGlobalEarnRegistry } from "src/interfaces/IGlobalEarnRegistry.sol";
+import { ICreationValidationManagerCore, StrategyId } from "src/interfaces/ICreationValidationManager.sol";
+import { IValidationManagersRegistryCore } from "src/interfaces/IValidationManagersRegistry.sol";
+import { BaseCreationValidation } from "../base/BaseCreationValidation.sol";
+
+abstract contract RegistryBasedCreationValidation is BaseCreationValidation, Initializable {
+  error InvalidData();
+
+  /// @notice The id for the Validation Managers Registry
+  bytes32 public constant VALIDATION_MANAGERS_REGISTRY = keccak256("VALIDATION_MANAGERS_REGISTRY");
+
+  /// @notice The address of the global registry
+  function globalRegistry() public view virtual returns (IGlobalEarnRegistry);
+
+  /// @notice The id assigned to this strategy
+  function strategyId() public view virtual returns (StrategyId);
+
+  // slither-disable-next-line naming-convention,dead-code
+  function _creationValidation_init(bytes calldata data) internal onlyInitializing {
+    (bytes memory registryData, bytes[] memory managersData) = abi.decode(data, (bytes, bytes[]));
+
+    IValidationManagersRegistryCore registry = _getRegistry();
+    registry.strategySelfConfigure(registryData);
+
+    ICreationValidationManagerCore[] memory managers = registry.getManagers(strategyId());
+    if (managersData.length != managers.length) {
+      revert InvalidData();
+    }
+
+    for (uint256 i = 0; i < managers.length; ++i) {
+      managers[i].strategySelfConfigure(managersData[i]);
+    }
+  }
+
+  // slither-disable-next-line naming-convention,dead-code
+  function _creationValidation_validate(address toValidate, bytes calldata data) internal override {
+    bytes[] memory dataArray = _decodeData(data);
+    StrategyId strategyId_ = strategyId();
+    IValidationManagersRegistryCore registry = _getRegistry();
+    ICreationValidationManagerCore[] memory managers = registry.getManagers(strategyId_);
+
+    if (dataArray.length != managers.length) {
+      revert InvalidData();
+    }
+
+    for (uint256 i = 0; i < managers.length; ++i) {
+      managers[i].validatePositionCreation(strategyId_, toValidate, msg.sender, dataArray[i]);
+    }
+  }
+
+  function _decodeData(bytes calldata data) private pure returns (bytes[] memory dataArray) {
+    return data.length > 0 ? abi.decode(data, (bytes[])) : new bytes[](0);
+  }
+
+  function _getRegistry() private view returns (IValidationManagersRegistryCore) {
+    return IValidationManagersRegistryCore(globalRegistry().getAddressOrFail(VALIDATION_MANAGERS_REGISTRY));
+  }
+}
---
+++ test/unit/strategies/layers/creation-validation/external/RegistryBasedCreationValidation.t.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.22;
+
+import { Test } from "forge-std/Test.sol";
+import {
+  ICreationValidationManagerCore,
+  RegistryBasedCreationValidation,
+  StrategyId,
+  IGlobalEarnRegistry,
+  IValidationManagersRegistryCore
+} from "src/strategies/layers/creation-validation/external/RegistryBasedCreationValidation.sol";
+import { CommonUtils } from "test/utils/CommonUtils.sol";
+
+contract RegistryBasedCreationValidationTest is Test {
+  bytes32 private constant GROUP_1 = keccak256("group1");
+  RegistryBasedCreationValidationInstance private validation;
+  IGlobalEarnRegistry private registry = IGlobalEarnRegistry(address(1));
+  IValidationManagersRegistryCore private managersRegistry = IValidationManagersRegistryCore(address(2));
+  ICreationValidationManagerCore private manager1 = ICreationValidationManagerCore(address(3));
+  ICreationValidationManagerCore private manager2 = ICreationValidationManagerCore(address(4));
+  StrategyId private strategyId = StrategyId.wrap(1);
+
+  function setUp() public virtual {
+    validation = new RegistryBasedCreationValidationInstance(registry, strategyId);
+    vm.mockCall(
+      address(registry),
+      abi.encodeWithSelector(IGlobalEarnRegistry.getAddressOrFail.selector, validation.VALIDATION_MANAGERS_REGISTRY()),
+      abi.encode(managersRegistry)
+    );
+    vm.mockCall(
+      address(managersRegistry),
+      abi.encodeWithSelector(IValidationManagersRegistryCore.getManagers.selector, strategyId),
+      abi.encode(_arrayOf(manager1, manager2))
+    );
+    vm.mockCall(
+      address(manager1), abi.encodeWithSelector(ICreationValidationManagerCore.validatePositionCreation.selector), ""
+    );
+    vm.mockCall(
+      address(manager2), abi.encodeWithSelector(ICreationValidationManagerCore.validatePositionCreation.selector), ""
+    );
+  }
+
+  function test_init_revertWhen_invalidData() public {
+    vm.expectRevert(abi.encodeWithSelector(RegistryBasedCreationValidation.InvalidData.selector));
+    // We pass an array of bytes of length 1, while there are two managers
+    validation.init(abi.encode("", new bytes[](1)));
+  }
+
+  function test_init() public {
+    bytes memory registryData = "12345";
+    bytes memory manager1Data = "67890";
+    bytes memory manager2Data = "12345";
+    bytes memory data = abi.encode(registryData, CommonUtils.arrayOf(manager1Data, manager2Data));
+    vm.expectCall(
+      address(managersRegistry),
+      abi.encodeWithSelector(IValidationManagersRegistryCore.strategySelfConfigure.selector, registryData)
+    );
+    vm.expectCall(
+      address(manager1),
+      abi.encodeWithSelector(ICreationValidationManagerCore.strategySelfConfigure.selector, manager1Data)
+    );
+    vm.expectCall(
+      address(manager2),
+      abi.encodeWithSelector(ICreationValidationManagerCore.strategySelfConfigure.selector, manager2Data)
+    );
+    validation.init(data);
+  }
+
+  function test_validate_revertWhen_invalidData() public {
+    vm.expectRevert(abi.encodeWithSelector(RegistryBasedCreationValidation.InvalidData.selector));
+    // We pass an array of bytes of length 1, while there are two managers
+    validation.validate(address(1), abi.encode(new bytes[](1)));
+  }
+
+  function test_validate() public {
+    address toValidate = address(10);
+    bytes memory manager1Data = "67890";
+    bytes memory manager2Data = "12345";
+    bytes memory data = abi.encode(CommonUtils.arrayOf(manager1Data, manager2Data));
+    vm.expectCall(
+      address(manager1),
+      abi.encodeWithSelector(
+        ICreationValidationManagerCore.validatePositionCreation.selector,
+        strategyId,
+        toValidate,
+        address(this),
+        manager1Data
+      )
+    );
+    vm.expectCall(
+      address(manager2),
+      abi.encodeWithSelector(
+        ICreationValidationManagerCore.validatePositionCreation.selector,
+        strategyId,
+        toValidate,
+        address(this),
+        manager2Data
+      )
+    );
+    validation.validate(toValidate, data);
+  }
+
+  function _arrayOf(
+    ICreationValidationManagerCore manager1_,
+    ICreationValidationManagerCore manager2_
+  )
+    internal
+    pure
+    returns (ICreationValidationManagerCore[] memory array)
+  {
+    array = new ICreationValidationManagerCore[](2);
+    array[0] = manager1_;
+    array[1] = manager2_;
+  }
+}
+
+contract RegistryBasedCreationValidationInstance is RegistryBasedCreationValidation {
+  IGlobalEarnRegistry private _registry;
+  StrategyId private _strategyId;
+
+  constructor(IGlobalEarnRegistry registry, StrategyId strategyId_) {
+    _registry = registry;
+    _strategyId = strategyId_;
+  }
+
+  function init(bytes calldata data) external initializer {
+    _creationValidation_init(data);
+  }
+
+  function validate(address sender, bytes calldata signature) external {
+    _creationValidation_validate(sender, signature);
+  }
+
+  function globalRegistry() public view virtual override returns (IGlobalEarnRegistry) {
+    return _registry;
+  }
+
+  function strategyId() public view virtual override returns (StrategyId) {
+    return _strategyId;
+  }
+}
