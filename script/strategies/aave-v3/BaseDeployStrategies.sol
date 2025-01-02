330ceb93571d0c98eca1c86a9f9f12cb4659a62d
0xKoaj <koaj@balmy.xyz>
2025-01-02 13:14:50 -0300

    feat: deplyoment scripts

---
+++ script/strategies/aave-v3/BaseDeployStrategies.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "../../BaseDeploy.sol";
+import { IEarnVault } from "@balmy/earn-core/interfaces/IEarnVault.sol";
+import { IGlobalEarnRegistry } from "src/interfaces/IGlobalEarnRegistry.sol";
+
+contract BaseDeployStrategies is BaseDeploy {
+  function setUp() public override {
+    super.setUp();
+    // Assume vault is already deployed
+  }
+
+  function deployAaveV3Strategy(
+    address aaveV3Pool,
+    address aaveV3Rewards,
+    address aToken,
+    string memory symbol,
+    bool isProtected
+  )
+    internal
+  {
+    address implementation = deployContract("V1_S_AAVEV3", abi.encodePacked(type(AaveV3Strategy).creationCode));
+    AaveV3StrategyFactory aaveV3StrategyFactory = AaveV3StrategyFactory(
+      deployContract(
+        "V1_F_AAVEV3", abi.encodePacked(type(AaveV3StrategyFactory).creationCode, abi.encode(implementation))
+      )
+    );
+    address vault = getDeployedAddress("V1_VAULT");
+    address globalRegistry = getDeployedAddress("V1_GLOBAL_REGISTRY");
+
+    bytes memory registryData = "";
+    bytes32 TOS_GROUP = keccak256("guardian_tos");
+    bytes memory manager1Data = abi.encode(TOS_GROUP);
+    bytes memory manager2Data = "";
+    if (isProtected) {
+      bytes32 SIGNER_GROUP = keccak256("signer_group");
+      manager2Data = abi.encode(SIGNER_GROUP);
+    }
+
+    bytes[] memory validationManagersStrategyData = new bytes[](2);
+    validationManagersStrategyData[0] = manager1Data;
+    validationManagersStrategyData[1] = manager2Data;
+    bytes memory creationValidationData = abi.encode(registryData, validationManagersStrategyData);
+    bytes memory guardianData = "";
+    bytes memory feesData = "";
+
+    (IEarnBalmyStrategy strategy, StrategyId strategyId) = aaveV3StrategyFactory.cloneStrategyAndRegister(
+      admin,
+      AaveV3StrategyData(
+        IEarnVault(vault),
+        IGlobalEarnRegistry(globalRegistry),
+        IAToken(aToken),
+        IAaveV3Pool(aaveV3Pool),
+        IAaveV3Rewards(aaveV3Rewards),
+        creationValidationData,
+        guardianData,
+        feesData,
+        string.concat(
+          "Earn returns with one of DeFi's most reliable lending markets. When you deposit ",
+          symbol,
+          " into Aave, your funds automatically generate interest by being lent to trusted borrowers - offering better returns than traditional savings. The protocol has handled over $100 billion in loans with a spotless security record, while keeping your funds available to withdraw at any time."
+        )
+      )
+    );
+    console2.log("Strategy:", address(strategy));
+    console2.log("Strategy ID:", StrategyId.unwrap(strategyId));
+  }
+}
---
+++ script/strategies/aave-v3/base/DeployStrategies.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "../BaseDeployStrategies.sol";
+import { IEarnVault } from "@balmy/earn-core/interfaces/IEarnVault.sol";
+import { IGlobalEarnRegistry } from "src/interfaces/IGlobalEarnRegistry.sol";
+
+contract DeployStrategies is BaseDeployStrategies {
+  function run() external {
+    vm.startBroadcast(deployerPrivateKey);
+
+    address aaveV3Pool = 0xA238Dd80C259a72e81d7e4664a9801593F98d1c5;
+    address aaveV3Rewards = 0xf9cc4F0D883F1a1eb2c253bdb46c254Ca51E1F44;
+
+    deployAaveV3Strategy(aaveV3Pool, aaveV3Rewards, 0x4e65fE4DbA92790696d040ac24Aa414708F5c0AB, "USDC", true);
+    deployAaveV3Strategy(aaveV3Pool, aaveV3Rewards, 0xD4a0e0b9149BCee3C920d2E00b5dE09138fd8bb7, "WETH", true);
+    deployAaveV3Strategy(aaveV3Pool, aaveV3Rewards, 0xBdb9300b7CDE636d9cD4AFF00f6F009fFBBc8EE6, "cbBTC", true);
+
+    vm.stopBroadcast();
+  }
+}
